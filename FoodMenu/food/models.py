21b93588cb23d2f94f1043e760d0200e1d733ad9
Gustavo Herrera <gherrera98r@gmail.com>
2025-03-27 16:27:59 -0600

    Added functions and improved style

--- FoodMenu/food/models.py
+++ FoodMenu/food/models.py
@@ -9,4 +9,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://kwalityindiancuisine.com/assets/img/food_images/placeholder.png")
+    
     
--- FoodMenu/food/views.py
+++ FoodMenu/food/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
+from .forms import ItemForm
 from django.template import loader
 
 # Create your views here.
@@ -17,3 +18,28 @@ def detail(request,item_id):
         'item':item
     }
     return render(request,'food/detail.html',context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request,'food/item-form.html',{'form':form, 'form_type': 'create'})
+
+def update_item(request,id):
+    item = Item.objects.get(id = id)
+    form = ItemForm(request.POST or None, instance= item)
+    
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+    return render(request,'food/item-form.html',{'form':form, 'item':item, 'form_type': 'update'} )
+    
+def delete_item(request,id):
+    item = Item.objects.get(id=id)
+    
+    if request.method == 'POST':
+        item.delete()
+        return redirect('index')
+    return render(request,'food/item-delete.html',{'item':item})
