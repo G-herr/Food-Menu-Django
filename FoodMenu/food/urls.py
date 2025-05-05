f572e1d6f89e633dd499622869d6963b4b05abf0
Gustavo Herrera <gherrera98r@gmail.com>
2025-05-05 14:09:32 -0600

    Added class-based views and refined frontend

--- FoodMenu/food/urls.py
+++ FoodMenu/food/urls.py
@@ -4,12 +4,13 @@ from django.urls import path
 #app_name='food'
 urlpatterns = [
     #/food/
-    path('',views.index,name='index'),
+    path('',views.IndexClassView.as_view(),name='index'),
     #/food/1
-    path('<int:item_id>/',views.detail,name='detail'),
-    path('item/',views.index,name='item'),
+    path('<int:pk>/',views.FoodDetail.as_view(),name='detail'),
+    #path('item/',views.index,name='item'),
+    
     # add items
-    path('add/', views.create_item,name='create_item'),
+    path('add/', views.CreateItem.as_view(),name='create_item'),
     #edit
     path('update/<int:id>/', views.update_item,name='update_item'),
     #delete
